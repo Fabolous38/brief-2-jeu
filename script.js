f66c6068251e35803f56d7edafc9c60683bfa790
Fabrice <fabrice.ndagijimana@gmail.com>
2024-02-12 22:54:56 +0100

    modification dans le fichier script.js au niveau des images

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onload = function() {
     for (let i = 0; i < 3; i++) {
         let choice = document.createElement("img");
         choice.id = choices[i];
-        choice.src = choices[i] + "images/.png";
+        choice.src = choices[i] + ".png";
         choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
@@ -18,11 +18,11 @@ window.onload = function() {
 
 function selectChoice() {
     you = this.id;
-    document.getElementById("your-choice").src = you + "images/.png";
+    document.getElementById("your-choice").src = you + ".png";
 
     //random for oppponent
     opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
-    document.getElementById("opponent-choice").src = opponent + "images/.png";
+    document.getElementById("opponent-choice").src = opponent + ".png";
 
     if (you == opponent) {
         yourScore += 1;
